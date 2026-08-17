979e72ca6812b6ffdda2dc6d7d85afc8137dc5cc
Bard1a-Bariri <bardia22a@gmail.com>
2026-08-17 18:18:07 +0000

    fix 7

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ if uploaded_files:
 
                     m_col1, m_col2 = st.columns(2)
                     with m_col1:
-                        st.metric("📅 Date / Time Taken", date_taken)
+                        st.metric("📅 Date / Time Taken", datetime.datetime.strptime(date_taken, "%Y:%m:%d %H:%M:%S").strftime("%b %d, %Y, %I:%M %p"))
                     with m_col2:
                         st.metric("📍 GPS Coordinates", coordinates)
 
