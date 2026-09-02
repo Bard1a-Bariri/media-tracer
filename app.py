fcc55d248b4940d5c18d7ffd0399c15f096818cd
Bard1a-Bariri <bardia22a@gmail.com>
2026-09-02 00:05:55 +0000

    fix

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import json
 import os
 import streamlit as st
 from PIL import Image
-import datetime
+from datetime import datetime
 
 from engine import run_full_analysis
 from network import generate_propagation_graph
